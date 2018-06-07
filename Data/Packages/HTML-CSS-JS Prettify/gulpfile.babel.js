55133dc0a2fb3cb8a40ca68d8445ef731fc6623c
Jesse <jesse@guojiang.tv>
2018-06-07 19:56:12 +0800

    add win32

--- Data/Packages/HTML-CSS-JS Prettify/gulpfile.babel.js
+++
@@ -1,6 +0,0 @@
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/publicdomain/zero/1.0/
-
-import './tasks/task-test';
-import './tasks/task-clean';
-import './tasks/task-build';
--- Data/Packages/User/addCurrentTime.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import sublime_plugin
-class AddCurrentTimeCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.run_command("insert_snippet",
-            {
-                "contents": "%s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+" jesse"
-            }
-        )
